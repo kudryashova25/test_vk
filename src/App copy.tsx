e0de7fd2282ad51acbb905bda4b62ec5eb360fd9
kudryashova25 <120773785+kudryashova25@users.noreply.github.com>
2024-11-29 12:10:42 +0300

    Update App copy.tsx
    переписала с видео "Урок 2. Знакомство с VKUI"

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -1,33 +1,22 @@
-import { useState, useEffect, ReactNode } from 'react';
-import bridge, { UserInfo } from '@vkontakte/vk-bridge';
-import { View, SplitLayout, SplitCol, ScreenSpinner } from '@vkontakte/vkui';
-import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
+import { View, Epic, SplitLayout, SplitCol} from '@vkontakte/vkui';
+import { AppTabBar, AppModalRoot } from 'components';
+import {useActiveVkuiLocation, usePopout} from '@vkontakte/vk-mini-apps-router';
 
-import { Persik, Home } from './panels';
-import { DEFAULT_VIEW_PANELS } from './routes';
+const { view: activeView, panel: activePanel = EPanel.DISHES } = useActiveVkuiLocation();
+const routerPopout = usePopout();
 
-export const App = () => {
-  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
-  const [fetchedUser, setUser] = useState<UserInfo | undefined>();
-  const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
-
-  useEffect(() => {
-    async function fetchData() {
-      const user = await bridge.send('VKWebAppGetUserInfo');
-      setUser(user);
-      setPopout(null);
-    }
-    fetchData();
-  }, []);
-
-  return (
-    <SplitLayout popout={popout}>
-      <SplitCol>
-        <View activePanel={activePanel}>
-          <Home id="home" fetchedUser={fetchedUser} />
-          <Persik id="persik" />
-        </View>
-      </SplitCol>
-    </SplitLayout>
-  );
-};
+<SplitLayout modal={<AppModalRoot />} popout={routerPopout}>
+  <SplitCol>
+    <Epic
+      activeStory={activeView ?? EView.DISHES}
+      tabbar={<AppTabBar activeStory={activeView as EView} />}
+      isHidden={activePanel === EPanel.DISH}
+    >
+      <View id={EView.DISHES} activePanel = {activePanel} >
+      <DishesPanel id={EPanel.DISHES} />
+      <DishesPanel id={EPanel.DISH} />
+      <OrderPanel id={EPanel.ORDER} />
+      </View>
+    </Epic>
+  </SplitCol>
+</SplitLayout>
