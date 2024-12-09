c8edcbfa444f3cb15afd2e611dd67241ec82d918
kudryashova25 <120773785+kudryashova25@users.noreply.github.com>
2024-12-09 15:24:14 +0300

    Update App.tsx
    17 строка у меня. 32 у вк.
    не забудь про импорты, ты их недописала

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
+// структура приложения
+
 import { View, Epic, SplitLayout, SplitCol} from '@vkontakte/vkui';
 import { AppTabBar, AppModalRoot } from 'components';
 import {useActiveVkuiLocation, usePopout} from '@vkontakte/vk-mini-apps-router';
 
-const { view: activeView, panel: activePanel = EPanel.DISHES } = useActiveVkuiLocation();
-const routerPopout = usePopout();
+import styles from './App.module.css';
 
-<SplitLayout modal={<AppModalRoot />} popout={routerPopout}>
-  <SplitCol>
-    <Epic
-      activeStory={activeView ?? EView.DISHES}
-      tabbar={<AppTabBar activeStory={activeView as EView} />}
-      isHidden={activePanel === EPanel.DISH}
-    >
-      <View id={EView.DISHES} activePanel = {activePanel} >
-      <DishesPanel id={EPanel.DISHES} />
-      <DishesPanel id={EPanel.DISH} />
-      <OrderPanel id={EPanel.ORDER} />
-      </View>
-    </Epic>
-  </SplitCol>
-</SplitLayout>
+const App = () => {
+  // Модуль 4. Разработка Урок 3. Роутинг #M4L3. Получение view и panel
+  const{ view: activeView, panel: activePanel = EPanel.DISHES } =
+    useActiveVkuiLocation();
+  const routerPopout = usePopout();
+  const dataContext = useContext(DataContext)
+}
+
+// продолжить отсюда
+
+/*
+  Модуль 4. Разработка Урок 2. Знакомство с VKUI #M4L2.
+  Структура приложения.
+*/
+return (
+  <SplitLayout modal={<AppModalRoot />} popout={routerPopout}>
+    <SplitCol className={StyleSheet.col}>
+      <Epic
+        activeStory={activeView ?? EView.DISHES}
+        tabbar={
+          <AppTabBar
+          activeStory={activeView as EView}
+          isHidden={activePanel === EPanel.DISH}
+          />
+        }
+        >
+          <View id={EView.DISHES} activePanel={activePanel}>
+            <DishesPanel id={EPanel.PROFILE} />
+            <AchievementsPanel id={EPanel.ACHIEVEMENTS} />
+            <UserOrdersPanel id={EPanel.USER_ORDERS} />
+            <NotificationPanel id={EPanel.NOTIFICATIONS} />
+          </View>
+          <View id={EView.RATING} activePanel={activePanel}>
+            <RatingPanel
+              id={EPanel.RATING}
+              adsBannnerPadding={adsBannerPadding}
+            />
+          </View>
+        </Epic>
+    </SplitCol>
+  </SplitLayout>
+);
