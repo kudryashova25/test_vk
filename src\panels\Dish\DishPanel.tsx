85e3e4eaa0ba3f89d5e257f82f7ca44f2e6e0f0f
kudryashova25 <120773785+kudryashova25@users.noreply.github.com>
2024-12-11 22:51:53 +0300

    Delete src\panels\Dish\DishPanel.tsx

--- src\panels\Dish\DishPanel.tsx
+++
@@ -1,8 +0,0 @@
-// src/panels/Dish/DishPanel.tsx
-import { useParams } from '@vkontakte/vk-mini-apps-router';
-
-const DishPanel = () => {
-    const params = useParams<'id'>();
-    // params.id вернет нам 123 при переходе на /dish/123
-    // ... 
-}
