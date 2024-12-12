62f49e3d7b5e46d56199807195be2160e44abe7a
kudryashova25 <120773785+kudryashova25@users.noreply.github.com>
2024-12-12 16:31:34 +0300

    Update DishList.tsx
    начала переписывать

--- src/panels/Dishes/components/DishList/DishList.tsx
+++ src/panels/Dishes/components/DishList/DishList.tsx
@@ -1,1 +1,16 @@
+// src/panels/Dishes/components/DishList/DishList.tsx 
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
+const DishList = ({ dishes, currentTab }: Props) => {
+    const routeNavigator = useRouteNavigator();
+
+    const navigateToDish = (id: number) => {
+        void routeNavigator.push(`/dish/${id}`);
+    };
+
+    return (
+        {dishes.map((dish) => (
+            <Dish dish={dish} currentTab={currentTab} key={dish.id} navigateToDish={navigateToDish} />
+        ))}
+    )
+}
